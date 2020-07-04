2709c264edb8b028c8518011b387f85154b6f0dc
Alex <boytunghc@gmail.com>
2020-07-04 16:30:19 +0700

    update assets folder

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,19 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+let assets = {
+    js: [
+        "resources/js/app.js"
+    ],
+    css: [
+        "resources/sass/app.scss"
+    ]
+}
+
+assets.js.forEach(source => {
+    mix.js(source, "public/assets/js");
+});
+
+assets.css.forEach(source => {
+    mix.sass(source, "public/assets/css");
+});
