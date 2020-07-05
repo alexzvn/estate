2ffc34e47314f007183b85b8085e8627ab8bba6e
Alex <boytunghc@gmail.com>
2020-07-05 14:33:42 +0700

    add permission group

---
+++ database/factories/PermissionGroupFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\PermissionGroup;
+use Faker\Generator as Faker;
+
+$factory->define(PermissionGroup::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_07_05_134856_create_permission_groups_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permission_groups', function (Blueprint $collection) {
+            $collection->unique('name');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permission_groups');
+    }
+}
