79b33e80155dc8000ba7990a2e268599ac878a30
Alex <boytunghc@gmail.com>
2020-07-06 15:25:36 +0700

    add filter province,  city, district, street

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,16 @@
 
 namespace App\Models;
 
+use App\EmptyClass;
+use App\Models\Traits\CanFilter;
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Builder;
 use Jenssegers\Mongodb\Eloquent\SoftDeletes;
+use App\Enums\PostMeta as Meta;
 
 class Post extends Model
 {
-    use SoftDeletes;
+    use SoftDeletes, CanFilter;
 
     protected $fillable = [
         'content', 'title', 'type', 'status'
@@ -20,5 +24,51 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function metas()
+    {
+        return $this->embedsMany(PostMeta::class);
+    }
+
+    public function getMetas()
+    {
+        return array_reduce($this->metas, function ($carry, $item)
+        {
+            $carry->{$item->name} = $item->value;
+            return $carry;
+        }, new EmptyClass);
+    }
+
+    public function filterProvince(Builder $builder, $value)
+    {
+        return $builder->whereHas(PostMeta::class, function (Builder $q) use ($value)
+        {
+            $q->where('name', Meta::Province)->where('value', $value);
+        });
+    }
+
+    public function filterCity(Builder $builder, $value)
+    {
+        return $builder->whereHas(PostMeta::class, function (Builder $q) use ($value)
+        {
+            $q->where('name', Meta::City)->where('value', $value);
+        });
+    }
+
+    public function filterDistrict(Builder $builder, $value)
+    {
+        return $builder->whereHas(PostMeta::class, function (Builder $q) use ($value)
+        {
+            $q->where('name', Meta::District)->where('value', $value);
+        });
+    }
+
+    public function filterStreet(Builder $builder, $value)
+    {
+        return $builder->whereHas(PostMeta::class, function (Builder $q) use ($value)
+        {
+            $q->where('name', Meta::Street)->where('value', $value);
+        });
     }
 }
