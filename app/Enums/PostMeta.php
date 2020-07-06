ea88dd0be7e66b1dadbf801d6e4ad7fc9bc10142
Alex <boytunghc@gmail.com>
2020-07-06 15:24:27 +0700

    add post meta model

---
+++ app/Enums/PostMeta.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static Province()
+ * @method static static City()
+ * @method static static District()
+ * @method static static Street()
+ */
+final class PostMeta extends Enum
+{
+    const Province = 'Address Province';
+    const City     = 'Address City';
+    const District = 'Address District';
+    const Street   = 'Address Street';
+}
