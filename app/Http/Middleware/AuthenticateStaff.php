00102038a8456f8849c04d7f2184dd4de0b356b2
Alex <boytunghc@gmail.com>
2020-07-06 17:18:30 +0700

    add middleware check staff

---
+++ app/Http/Middleware/AuthenticateStaff.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Enums\Role;
+use Closure;
+
+class AuthenticateStaff
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (! ($user = $request->user())) {
+            return abort(403);
+        }
+
+        if ($user->hasAllRoles([Role::SuperAdmin, Role::Staff])) {
+            return $next($request);
+        }
+
+        return abort(403);
+    }
+}
