e0273169cec42e786e6d7e342b1948c581a1f97a
Alex <boytunghc@gmail.com>
2020-07-06 22:07:13 +0700

    add text while loading dashboard

--- resources/views/dashboard/layouts/loader.blade.php
+++ resources/views/dashboard/layouts/loader.blade.php
@@ -1,5 +1,10 @@
     <!-- BEGIN LOADER -->
-    <div id="load_screen"> <div class="loader"> <div class="loader-content">
-        <div class="spinner-grow align-self-center"></div>
-    </div></div></div>
+    <div id="load_screen">
+        <div class="loader">
+            <div class="loader-content text-center">
+                <div class="spinner-grow align-self-center"></div>
+                <h3 class="mt-5">Chờ chút nào...</h3>
+            </div>
+        </div>
+    </div>
     <!--  END LOADER -->
