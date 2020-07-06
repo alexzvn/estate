191031eb5c520d413249f01e09be40f2ed070078
Alex <boytunghc@gmail.com>
2020-07-06 13:53:56 +0700

    add status post_enum

---
+++ app/Enums/PostStatus.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static Draft()
+ * @method static static Pending()
+ * @method static static Published()
+ */
+final class PostStatus extends Enum
+{
+    const Draft     = 0;
+
+    /**
+     * Waiting post need action from admin
+     */
+    const Pending   = 1;
+
+    const Published = 2;
+}
