f0eecea7fe29ff5b6da662a4e34ec8d0b57d242e
Alex <boytunghc@gmail.com>
2020-07-08 16:11:25 +0700

    fix method relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,7 @@ class User extends Authenticatable
 
     public function posts()
     {
-        return $this->embedsMany(Post::class);
+        return $this->hasMany(Post::class);
     }
 
     public function isAdmin()
