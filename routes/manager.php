85a7feca0cd1d21e17ceb961752ae072130c215c
Alex <boytunghc@gmail.com>
2020-07-09 15:44:12 +0700

    add route post/user

--- routes/manager.php
+++ routes/manager.php
@@ -23,3 +23,19 @@ Route::group(['prefix' => 'role', 'namespace' => 'Manager\Role'], function () {
     Route::post('/{id}/update', 'RoleController@index')->name('manager.role.update');
     Route::get('/{id}/delete', 'RoleController@index')->name('manager.role.delete');
 });
+
+Route::group(['prefix' => 'post', 'namespace' => 'Manager\Post'], function () {
+    Route::get('/', 'PostController@index')->name('manager.post');
+    Route::get('/create', 'PostController@index')->name('manager.post.create');
+    Route::get('/{id}/view', 'PostController@index')->name('manager.post.view');
+    Route::post('/{id}/update', 'PostController@index')->name('manager.post.update');
+    Route::get('/{id}/delete', 'PostController@index')->name('manager.post.delete');
+});
+
+Route::group(['prefix' => 'user', 'namespace' => 'Manager\User'], function () {
+    Route::get('/', 'UserController@index')->name('manager.user');
+    Route::get('/create', 'UserController@index')->name('manager.user.create');
+    Route::get('/{id}/view', 'UserController@index')->name('manager.user.view');
+    Route::post('/{id}/update', 'UserController@index')->name('manager.user.update');
+    Route::get('/{id}/delete', 'UserController@index')->name('manager.user.delete');
+});
