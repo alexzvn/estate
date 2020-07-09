7265cac97060775bb2203e2da4d0207a692833cc
Alex <boytunghc@gmail.com>
2020-07-09 14:20:15 +0700

    update title

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -1,43 +1,35 @@
         <!--  BEGIN SIDEBAR  -->
+        
         <div class="sidebar-wrapper sidebar-theme">
             
             <nav id="sidebar">
                 <div class="shadow-bottom"></div>
 
                 <ul class="list-unstyled menu-categories" id="accordionExample">
-                    <li class="menu">
-                        <a href="#starter-kit" data-active="true" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle">
-                            <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-terminal"><polyline points="4 17 10 11 4 5"></polyline><line x1="12" y1="19" x2="20" y2="19"></line></svg>
-                                <span>Starter Kit</span>
-                            </div>
-                            <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
-                            </div>
-                        </a>
-                        <ul class="submenu list-unstyled collapse show" id="starter-kit" data-parent="#accordionExample" style="">
-                            <li class="active">
-                                <a href="starter_kit_blank_page.html"> Blank Page </a>
-                            </li>
-                            <li>
-                                <a href="starter_kit_breadcrumbs.html"> Breadcrumbs </a>
-                            </li>
-                            <li>
-                                <a href="starter_kit_boxed.html"> Boxed </a>
-                            </li>
-                            <li>
-                                <a href="starter_kit_alt_menu.html"> Alternate Menu </a>
-                            </li>
-                        </ul>
-                    </li>
 
                     <li class="menu">
-                        <a href="javascript:void(0);" aria-expanded="false" class="dropdown-toggle">
-                            <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                                <span> Menu 1</span>
+                        <a href="{{ route('manager') }}" @active('manager', 'data-active="true"') aria-expanded="false" class="dropdown-toggle">
+                            <div>
+                                <i data-feather="home"></i> <span>Trang chủ</span>
                             </div>
                         </a>
+                    </li>
+    
+                    <li class="menu">
+                        <a href="#role" @active('manager.role', 'data-active="true"') data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                            <div class="">
+                                <i data-feather="award"></i>
+                                <span>Vai trò</span>
+                            </div>
+                            <div>
+                                <i data-feather="chevron-right"></i>
+                            </div>
+                        </a>
+                        <ul class="submenu list-unstyled collapse @active('manager.role', 'show')" id="role" data-parent="#accordionExample" style="">
+                            <li class="@active('manager.role')">
+                                <a href="{{ route('manager.role') }}"> Tất cả vai trò </a>
+                            </li>
+                        </ul>
                     </li>
 
                     <li class="menu">
