76647754b7c70bf596978724ed2dde3617c6f7a5
Alex <boytunghc@gmail.com>
2020-07-09 15:45:50 +0700

    update content on sidebar

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -33,53 +33,47 @@
                     </li>
 
                     <li class="menu">
-                        <a href="#submenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <a href="#submenu" data-toggle="collapse" @active('manager.post', 'data-active="true"') aria-expanded="false" class="dropdown-toggle">
                             <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-airplay"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1"></path><polygon points="12 15 17 21 7 21 12 15"></polygon></svg>
-                                <span> Menu 2</span>
+                                <i data-feather="file-text"></i>
+                                <span>Tin BĐS</span>
                             </div>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                                <i data-feather="chevron-right"></i>
                             </div>
                         </a>
-                        <ul class="collapse submenu list-unstyled" id="submenu" data-parent="#accordionExample">
+                        <ul class="collapse submenu list-unstyled @active('manager.post', 'show')" id="submenu" data-parent="#accordionExample">
                             <li>
-                                <a href="javascript:void(0);"> Submenu 1 </a>
+                                <a href="javascript:void(0);"> Tin xin phí </a>
                             </li>
                             <li>
-                                <a href="javascript:void(0);"> Submenu 2 </a>
-                            </li>                           
+                                <a href="javascript:void(0)">Tin cần thuê</a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0);"> Tin chờ duyệt </a>
+                            </li>
                         </ul>
                     </li>
 
                     <li class="menu">
-                        <a href="#submenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+                        <a href="#submenu2" @active('manager.user', 'data-active="true"') data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
                             <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file"><path d="M13 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V9z"></path><polyline points="13 2 13 9 20 9"></polyline></svg>
-                                <span> Menu 3</span>
+                                <i data-feather="users"></i>
+                                <span> Thành viên</span>
                             </div>
                             <div>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg>
+                                <i data-feather="chevron-right"></i>
                             </div>
                         </a>
-                        <ul class="collapse submenu list-unstyled" id="submenu2" data-parent="#accordionExample">
+                        <ul class="collapse submenu list-unstyled @active('manager.user', 'show')" id="submenu2" data-parent="#accordionExample">
                             <li>
-                                <a href="javascript:void(0);"> Submenu 1 </a>
+                                <a href="javascript:void(0);"> Tất cả </a>
                             </li>
+                            @foreach ($roles as $item)
                             <li>
-                                <a href="#sm2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Submenu 2 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
-                                <ul class="collapse list-unstyled sub-submenu" id="sm2" data-parent="#submenu2"> 
-                                    <li>
-                                        <a href="javascript:void(0);"> Sub-Submenu 1 </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);"> Sub-Submenu 2 </a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);"> Sub-Submenu 3 </a>
-                                    </li>
-                                </ul>
+                                <a href="javascript:void(0);"> {{ $item->name }} </a>
                             </li>
+                            @endforeach
                         </ul>
                     </li>
                 </ul>
