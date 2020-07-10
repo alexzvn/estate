620d8a82bb48f7857b0e4fccfb588bb4af7b5ddd
Alex <boytunghc@gmail.com>
2020-07-10 18:19:54 +0700

    add item for nav

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,30 @@
-<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+<nav class="navbar navbar-expand-md navbar-light shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+
+        <div class="d-flex">
+            @auth
+            <a class="nav-link btn btn-warning d-block d-md-none mr-3" href="{{ route('register') }}"> <i class="far fa-edit"></i> Đăng Bài</a>
+            @endauth
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-
+            @auth
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i class="fas fa-landmark"></i> Tôi bán</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"><i class="far fa-comments"></i> Hỗ trợ</a>
+            </li>
+            @endauth
             </ul>
 
             <!-- Right Side Of Navbar -->
@@ -21,14 +35,19 @@
                         <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                     </li>
                     @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link btn btn-primary" style="color: white" href="{{ route('register') }}">Đăng ký ngay</a>
-                        </li>
+                    <li class="nav-item">
+                        <a class="nav-link btn btn-primary" style="color: white" href="{{ route('register') }}">Đăng ký ngay</a>
+                    </li>
                     @endif
                 @else
+                    <li class="nav-item d-none d-sm-block mr-md-3">
+                        <a class="nav-link btn btn-warning" style="color: black;" href="{{ route('register') }}">
+                            <i class="far fa-edit"></i> Đăng Tin
+                        </a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            Xin chào, {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
