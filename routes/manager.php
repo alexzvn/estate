d101fe43a46c670840fe418aea29000c1be38b3c
Alex <boytunghc@gmail.com>
2020-07-11 13:17:55 +0700

    update route

--- routes/manager.php
+++ routes/manager.php
@@ -26,7 +26,7 @@ Route::group(['prefix' => 'role', 'namespace' => 'Manager\Role'], function () {
 
 Route::group(['prefix' => 'post', 'namespace' => 'Manager\Post'], function () {
     Route::get('/', 'PostController@index')->name('manager.post');
-    Route::get('/create', 'PostController@index')->name('manager.post.create');
+    Route::get('/create', 'PostController@create')->name('manager.post.create');
     Route::get('/{id}/view', 'PostController@index')->name('manager.post.view');
     Route::post('/{id}/update', 'PostController@index')->name('manager.post.update');
     Route::get('/{id}/delete', 'PostController@index')->name('manager.post.delete');
