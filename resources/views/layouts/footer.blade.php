28bebba0ba1e5122076e3c3909f9b0524ec08530
Alex <boytunghc@gmail.com>
2020-07-13 20:01:53 +0700

    update text footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,16 +1,16 @@
 <footer class="py-3 mt-5" style="background-color: rgb(231, 244, 244); outline: solid 4px #5886ae;">
     <div class="container mt-3">
-        <div class="row">
-            <div class="col-md-5">
-                <p><strong>© Công Ty Cổ Phần Công Nghệ Và Dịch Vụ Việt Hàn (VietHan Jsc)</strong></p>
-                <p>Trụ sở : Số 6/4 Nadyne, đô thị Parkcity, quận Hà Đông, Hà Nội.
-                    VP HN : Số 26 LK 2A, Nguyễn Văn Trỗi, quận Hà Đông, Hà Nội.
-                    VP HCM : Block 2A/301 cc Phú Mỹ, Hoàng Quốc Việt, Q7, HCM.
-                    ĐKKD Số: 0109152480 cấp ngày 9/4/2020.</p>
-                <p>" VHHome.vn tổng hợp tin bất động sản bởi chương trình máy tính. Chúng tôi không sở hữu các thông tin này. "</p>
+        <div class="row h-100">
+            <div class="col-md-5 my-auto">
+                {{-- <p><strong>© Công Ty Cổ Phần Công Nghệ Và Dịch Vụ Việt Hàn (VietHan Jsc)</strong></p> --}}
+                <p><a href="{{ request()->url() }}" style="color: black; text-transform: capitalize;">
+                    <strong>{{ request()->getHttpHost() }}</strong></a> là công cụ hỗ trợ tối ưu nhất giúp các nhà môi giới, cá nhân có nhu cầu tìm kiếm, giao dịch nhà đất tổng hợp, chọn lọc và phân loại được những thông tin bất động sản chính chủ nhanh chóng, thuận tiện và chính xác nhất. </p>
+                <p><a href="{{ request()->url() }}" style="color: black; text-transform: capitalize;">
+                    <strong>{{ request()->getHttpHost() }}</strong></a> tổng hợp tất cả tin bất động sản bởi chương trình máy tính, Chúng tôi không sở hữu và chịu trách nhiệm thông tin này.
+                    Trụ sở: CT1 Bắc Hà, Dự Án C14 BCA Trung Văn, Nam từ Liêm, TP Hà Nội</p>
             </div>
     
-            <div class="offset-md-1 col-md-6">
+            <div class="offset-md-1 col-md-6 my-auto">
                 <div class="card">
                     <div class="card-body">
                         <h4>Tư vấn & kinh doanh</h4>
@@ -18,22 +18,22 @@
                         <div class="row">
                             <div class="col-sm-6">
                                 <p>
-                                    <strong>Ms Thanh Ngân</strong><br>
-                                    ĐT/Zalo: <strong>0896.523.999</strong><br>
-                                    Email: Ngandt_vh@vipost.vn
+                                    <strong>Ms. Thủy Ngân</strong><br>
+                                    ĐT/Zalo: <strong>033.5366.224</strong><br>
+                                    Thuyngan@nguonchinhchu.vn
                                 </p>
                             </div>
                             <div class="col-sm-6">
                                 <p>
-                                    <strong>Ms Thanh Ngân</strong><br>
-                                    ĐT/Zalo: <strong>0896.523.999</strong><br>
-                                    Email: Ngandt_vh@vipost.vn
+                                    <strong>Ms. Thủy Ngân</strong><br>
+                                    ĐT/Zalo: <strong>096.55.33.958</strong><br>
+                                    Thequang@nguonchinhchu.vn
                                 </p>
                             </div>
                         </div>
 
                         <hr>
-
+                        <p class="mb-0" style="font-size: 1.5rem; font-family: initial;">Hotline: <strong>096.55.33.958</strong></p>
                     </div>
                 </div>
             </div>
