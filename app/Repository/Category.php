fb125f5a2a98edab54287bfe93dc38a94d7a7d79
Alex Jim <boytunghc@gmail.com>
2020-07-27 17:30:06 +0700

    add flat function

--- app/Repository/Category.php
+++ app/Repository/Category.php
@@ -2,11 +2,27 @@
 
 namespace App\Repository;
 
+use Illuminate\Support\Collection;
 use App\Models\Category as ModelsCategory;
 
 class Category extends BaseRepository
 {
     public function __construct(ModelsCategory $model) {
         $this->model = $model;
+    }
+
+    /**
+     * Flatten category parent & childen
+     *
+     * @param Collection $categories
+     * @return \App\Models\Category[]|Illuminate\Support\Collection
+     */
+    public static function flat(Collection $categories)
+    {
+        return $categories->reduce(function (Collection $carry, $item) {
+            $carry = $carry->concat($item);
+
+            return $item->children ? $carry->concat($item->children) : $carry;
+        }, collect());
     }
 }
