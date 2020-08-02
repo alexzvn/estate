9fd8d4cadeae7e708c7792773934af12e6ac621e
Alex <boytunghc@gmail.com>
2020-08-02 20:14:50 +0700

    add reserver post schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,9 +2,11 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ReverserPost;
 use App\Console\Commands\SyncPermissionConfig;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,7 +16,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        SyncPermissionConfig::class
+        SyncPermissionConfig::class,
+        ReverserPost::class
     ];
 
     /**
@@ -26,6 +29,13 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function ()
+        {
+            if (now()->today()->addHours(8)->greaterThan(now())) {
+                Artisan::call('post:reverser --item=3');
+            }
+        })->everyTenMinutes();
     }
 
     /**
