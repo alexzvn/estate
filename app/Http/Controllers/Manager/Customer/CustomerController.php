2721b22480bc896c5d40fc6184efd6924b5484fe
Alex <boytunghc@gmail.com>
2020-08-04 09:54:06 +0700

    fix customer order redirect

--- app/Http/Controllers/Manager/Customer/CustomerController.php
+++ app/Http/Controllers/Manager/Customer/CustomerController.php
@@ -184,7 +184,7 @@ class CustomerController extends Controller
 
         $order->plans()->sync($request->plans ?? []);
 
-        return redirect(route('manager.customer.view', ['id' => $order->id]));
+        return redirect(route('manager.order.view', ['id' => $order->id]));
     }
 
     private function assignCustomerToUser($customer, $userId = null)
