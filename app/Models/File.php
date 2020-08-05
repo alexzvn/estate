a162ac76e23a1fc228393bb03308d02c1fdf1efc
Alex <boytunghc@gmail.com>
2020-08-05 11:32:46 +0700

    add fillable, fix mongo model

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,9 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = ['name', 'path'];
 }
