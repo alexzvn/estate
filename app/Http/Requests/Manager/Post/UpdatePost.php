4c6a2a4453a6ee644b68b5996cc2665cfdde799c
Alex <boytunghc@gmail.com>
2020-08-05 11:40:11 +0700

    update vaildate

--- app/Http/Requests/Manager/Post/UpdatePost.php
+++ app/Http/Requests/Manager/Post/UpdatePost.php
@@ -28,17 +28,19 @@ class UpdatePost extends FormRequest
     {
         return [
             'post_content' => 'required',
-            'title'    => 'required',
-            'phone'    => 'required|regex:/^[0-9_.]+$/',
-            'price'    => 'required|regex:/^[0-9,.]+$/',
-            'category' => 'required|exists:categories,_id',
-            'province' => 'nullable|exists:provinces,_id',
-            'district' => 'nullable|exists:districts,_id',
-            'type'     => [
+            'title'     => 'required',
+            'phone'     => 'required|regex:/^[0-9_.]+$/',
+            'price'     => 'required|regex:/^[0-9,.]+$/',
+            'category'  => 'required|exists:categories,_id',
+            'province'  => 'nullable|exists:provinces,_id',
+            'district'  => 'nullable|exists:districts,_id',
+            'images'    => 'nullable|array|image',
+            'image_ids' => 'nullable|array|exists:files,_id',
+            'type'      => [
                 'nullable',
                 Rule::in(PostType::getValues())
             ],
-            'status'   => [
+            'status'    => [
                 'required',
                 Rule::in(PostStatus::getValues())
             ]
--- resources/views/dashboard/post/edit.blade.php
+++ resources/views/dashboard/post/edit.blade.php
@@ -160,7 +160,7 @@
                     <div class="custom-file-container" data-upload-id="mySecondImage">
                         <label>Chọn ảnh đại diện <a href="javascript:void(0)" class="custom-file-container__image-clear" title="Clear Image">x</a></label>
                         <label class="custom-file-container__custom-file" >
-                            <input type="file" class="custom-file-container__custom-file__custom-file-input" multiple>
+                            <input type="file" name="images[]" class="custom-file-container__custom-file__custom-file-input" multiple>
                             <input type="hidden" name="MAX_FILE_SIZE" value="10485760" />
                             <span class="custom-file-container__custom-file__custom-file-control"></span>
                         </label>
