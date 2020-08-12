58295bb370645ea7b67d7592d1d8be911192ed53
Alex <boytunghc@gmail.com>
2020-08-12 14:53:56 +0700

    hide new post by customer

--- resources/views/customer/layouts/header.blade.php
+++ resources/views/customer/layouts/header.blade.php
@@ -5,12 +5,12 @@
         </a>
 
         <div class="d-flex">
-            @auth
+            {{-- @auth
             <a class="nav-link btn btn-warning d-block d-md-none mr-3" href="javascript:void(0)" data-toggle="modal" data-target="#create-post-modal">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                 Đăng Bài
             </a>
-            @endauth
+            @endauth --}}
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -30,12 +30,12 @@
 
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item d-none d-sm-block mr-md-3">
+                {{-- <li class="nav-item d-none d-sm-block mr-md-3">
                     <a class="nav-link btn btn-warning" style="color: black;" data-toggle="modal" data-target="#create-post-modal">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                         Đăng Tin
                     </a>
-                </li>
+                </li> --}}
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         Xin chào, {{ Auth::user()->name }} <span class="caret"></span>
