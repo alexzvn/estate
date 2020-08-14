5b75be6f87adcc7ce62e96dc9279179efcbd6cb8
Alex <boytunghc@gmail.com>
2020-08-14 18:31:07 +0700

    remove old class

--- app/Http/Controllers/Manager/Customer/CustomerController.php
+++ app/Http/Controllers/Manager/Customer/CustomerController.php
@@ -12,9 +12,7 @@ use App\Http\Requests\Manager\Customer\StoreCustomer;
 use App\Http\Requests\Manager\Customer\UpdateCustomer;
 use App\Http\Requests\Manager\Customer\Order\StoreOrder;
 use App\Models\Order;
-use App\Models\User as ModelsUser;
 use App\Repository\Role;
-use App\Repository\Subscription;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Auth;
 
