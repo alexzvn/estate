8ac5ccc8a62ff5ec472898f2e152d3f06fec2aa4
Alex <boytunghc@gmail.com>
2020-08-20 19:31:24 +0700

    add switch on/off reverse post

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\ReverserPost;
 use App\Console\Commands\SyncPermissionConfig;
+use App\Repository\Setting;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Artisan;
@@ -28,10 +29,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('post:reverser --item=3')
-            ->everyTenMinutes()
-            ->between('7:00', '22:00')
-            ->appendOutputTo(storage_path('logs/schedule.log'));
+        $setting = app(Setting::class);
+
+        if ($setting->config('post.reverse', false)) {
+            $schedule->command('post:reverser --item=3')
+                ->everyTenMinutes()
+                ->between('7:00', '22:00')
+                ->appendOutputTo(storage_path('logs/schedule.log'));
+        }
     }
 
     /**
