c8f196651b49a246323c068b288a78c31bef4a14
Alex <boytunghc@gmail.com>
2020-08-20 10:58:18 +0700

    apply limit paginate for customer only

--- resources/views/customer/home.blade.php
+++ resources/views/customer/home.blade.php
@@ -18,7 +18,7 @@
                         @else
                             @include('customer.components.posts-table', ['posts' => $posts])
                             <div class="d-flex justify-content-center">
-                                {{ $posts->withQueryString()->onEachSide(1)->links() }}
+                                {{ $posts->withQueryString()->onEachSide(1)->links('customer.layouts.paginate') }}
                             </div>
                         @endif
 
