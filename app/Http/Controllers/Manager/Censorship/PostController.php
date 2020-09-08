e535ad31502206ff2047f47efb7e1486d8084b09
Alex <boytunghc@gmail.com>
2020-09-08 20:30:58 +0700

    update query

--- app/Http/Controllers/Manager/Censorship/PostController.php
+++ app/Http/Controllers/Manager/Censorship/PostController.php
@@ -21,13 +21,7 @@ class PostController extends Controller
 
         $post = $this->filterTracking($post, $request)
             ->published()
-            ->whereHas('metas', function ($builder)
-            {
-                $builder->whereHas('trackingPost', function ($builder)
-                {
-                    $builder->withoutWhitelist();
-                });
-            });
+            ->withoutWhitelist();
 
         return view('dashboard.censorship.index', [
             'posts' => $post->paginate(40),
@@ -37,22 +31,19 @@ class PostController extends Controller
 
     public function filterTracking($post, Request $request)
     {
-        return $post->whereHas('metas', function ($builder) use ($request)
+        return $post->whereHas('tracking', function ($builder) use ($request)
         {
-            $builder->whereHas('trackingPost', function ($builder) use ($request)
-            {
-                if ($request->categories_unique) {
-                    $builder->where('categories_unique', '>', (int) $request->categories_unique);
-                }
+            if ($request->categories_unique) {
+                $builder->where('categories_unique', '>', (int) $request->categories_unique);
+            }
 
-                if ($request->district_unique) {
-                    $builder->where('district_unique', '>', (int) $request->district_unique);
-                }
+            if ($request->district_unique) {
+                $builder->where('district_unique', '>', (int) $request->district_unique);
+            }
 
-                if ($request->seen) {
-                    $builder->where('seen', '>', $request->seen ? (int) $request->seen : 1);
-                }
-            });
+            if ($request->seen) {
+                $builder->where('seen', '>', $request->seen ? (int) $request->seen : 1);
+            }
         });
     }
 
