ea80792ffdb11311fc5fe39ee8ed3b1f629e168b
Alex <boytunghc@gmail.com>
2020-09-08 19:32:42 +0700

    remove meta observer

--- app/Providers/RegisterObserverServiceProvider.php
+++ app/Providers/RegisterObserverServiceProvider.php
@@ -4,10 +4,8 @@ namespace App\Providers;
 
 use App\Models\Blacklist;
 use App\Models\Post;
-use App\Models\PostMeta;
 use App\Models\User;
 use App\Observers\BlacklistObserver;
-use App\Observers\MetaObserver;
 use App\Observers\PostObserver;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
