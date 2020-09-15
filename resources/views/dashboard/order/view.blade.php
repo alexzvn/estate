8c3012e39461c3bc559afac54f8d6c5af6ef2ad4
Alex <41188285+alexzvn@users.noreply.github.com>
2020-09-15 09:36:01 +0700

    revent permission

--- resources/views/dashboard/order/view.blade.php
+++ resources/views/dashboard/order/view.blade.php
@@ -120,7 +120,7 @@ $manual  = $order->manual !== null && $order->manual;
                             </div>
                         </div>
 
-                        @if (! $order->verified && request()->user()->can('*'))
+                        @if (! $order->verified)
                         <div class="form-check">
                             <label class="form-check-label">
                               <input type="checkbox" class="form-check-input" name="verified" id="verified" value="1">
