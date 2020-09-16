9a9fb9f25e29a82f5e606e21fa7f5f0200aedc1e
Alex <boytunghc@gmail.com>
2020-09-16 19:14:37 +0700

    add find users by permission name

--- app/Repository/Permission.php
+++ app/Repository/Permission.php
@@ -9,4 +9,18 @@ class Permission extends BaseRepository
     public function __construct(ModelsPermission $model) {
         $this->model = $model;
     }
+
+    /**
+     * Get list users by permission
+     *
+     * @return \Illuminate\Database\Eloquent\Collection|App\Models\User[]|array
+     */
+    public static function findUsersHasPermission(string $perm)
+    {
+        $staffGroup = static::findByName($perm)->users;
+
+        $adminGroup = static::findByName('*')->users;
+
+        return $adminGroup->push(...$staffGroup)->unique('id');
+    }
 }
