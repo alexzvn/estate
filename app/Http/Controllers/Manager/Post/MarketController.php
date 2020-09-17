c93be4bad9aac22f1bfac975b89852a4685f6549
Alex <boytunghc@gmail.com>
2020-09-17 21:15:01 +0700

    add method update

--- app/Http/Controllers/Manager/Post/MarketController.php
+++ app/Http/Controllers/Manager/Post/MarketController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Manager\Post;
 
 use App\Enums\PostType;
+use App\Http\Requests\Manager\Post\Market\UpdatePost;
 use App\Repository\Post;
+use App\Services\System\Post\PostService;
 use Illuminate\Http\Request;
 
 class MarketController extends PostController
@@ -20,5 +22,16 @@ class MarketController extends PostController
         $this->shareCategoriesProvinces();
 
         return view('dashboard.post.market.list', compact('posts'));
+    }
+
+    public function updateMarket(string $id, UpdatePost $request)
+    {
+        $post = Post::findOrFail($id);
+
+        $post = PostService::update($post, $request->all());
+
+        $this->syncUploadFiles($post, $request);
+
+        return back()->with('success', 'Cập nhật thành công');
     }
 }
