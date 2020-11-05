f8df16c0bbb20cdc3858933d1d3edbfffd9b1c75
Alex <boytunghc@gmail.com>
2020-11-05 18:05:35 +0700

    add nullable

--- database/migrations/2020_08_02_212437_create_logs_table.php
+++ database/migrations/2020_08_02_212437_create_logs_table.php
@@ -15,7 +15,7 @@ class CreateLogsTable extends Migration
     {
         Schema::create('logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id');
+            $table->foreignId('user_id')->nullable();
             $table->string('link', 2048)->nullable();
             $table->string('content', 2000);
             $table->timestamps();
