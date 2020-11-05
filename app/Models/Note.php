52fb2a05fc4b30d4f7fb8565edd097704b75e87c
Alex <boytunghc@gmail.com>
2020-11-05 18:37:00 +0700

    update note relationship

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Note extends Model
 {
     protected $fillable = ['content'];
+
+    public function notable()
+    {
+        return $this->morphTo();
+    }
 }
