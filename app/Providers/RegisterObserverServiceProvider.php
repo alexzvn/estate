06ec36b5675a46d79642013e6179347ae457fec3
Alex <boytunghc@gmail.com>
2020-11-19 23:54:33 +0700

    add note observer

--- app/Providers/RegisterObserverServiceProvider.php
+++ app/Providers/RegisterObserverServiceProvider.php
@@ -3,9 +3,11 @@
 namespace App\Providers;
 
 use App\Models\Blacklist;
+use App\Models\Note;
 use App\Models\Post;
 use App\Models\User;
 use App\Observers\BlacklistObserver;
+use App\Observers\NoteObserver;
 use App\Observers\PostObserver;
 use App\Observers\UserObserver;
 use Illuminate\Support\ServiceProvider;
@@ -19,6 +21,7 @@ class RegisterObserverServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Note::observe(NoteObserver::class);
         Post::observe(PostObserver::class);
         User::observe(UserObserver::class);
         Blacklist::observe(BlacklistObserver::class);
