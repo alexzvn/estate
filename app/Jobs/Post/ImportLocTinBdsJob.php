2c371b2981f8a10fe6bd4effbe399d34c6c0dd3e
Alex <boytunghc@gmail.com>
2021-01-18 19:17:10 +0700

    implement queue

--- app/Jobs/Post/ImportLocTinBdsJob.php
+++ app/Jobs/Post/ImportLocTinBdsJob.php
@@ -12,7 +12,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use stdClass;
 
-class ImportLocTinBdsJob
+class ImportLocTinBdsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
