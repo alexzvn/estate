1ed69f39c84ed48b7817e1d0ef295b51d9cb69d9
Alex <boytunghc@gmail.com>
2021-02-08 19:10:47 +0700

    catch missing address or empty category post

--- app/Jobs/Post/ImportTccJob.php
+++ app/Jobs/Post/ImportTccJob.php
@@ -28,8 +28,14 @@ class ImportTccJob extends ImportPostJob
 
         $date = array_reverse(explode('/', $this->post->createDate));
 
+        $category = $this->getCategory();
         $province = Province::where('name', 'regexp', "/{$this->post->province}/")->first();
         $district = District::where('name', 'regexp', "/{$this->post->district}/")->first();
+
+        if ($this->checkItems($province, $district, $category)) {
+            $this->setReport((array) $this->post);
+            throw new \Exception("Province, district or category is empty", 1);
+        }
 
         $post = Online::create([
             'title'       => $this->post->title,
@@ -39,7 +45,7 @@ class ImportTccJob extends ImportPostJob
             'status'      => PostStatus::Published,
             'type'        => PostType::Online,
             'source'      => PostSource::TinChinhChu,
-            'categories'  => $this->getCategory(),
+            'categories'  => $category,
             'price'       => $this->post->price,
             'phone'       => $this->post->phone,
             'province_id' => $province->id ?? null,
@@ -53,6 +59,23 @@ class ImportTccJob extends ImportPostJob
         $post->save();
     }
 
+    protected function checkItems(...$items)
+    {
+        foreach ($items as $item) {
+            if (empty($item)) return true;
+        }
+
+        return false;
+    }
+
+    protected function setReport(array $context)
+    {
+        \Sentry\configureScope(function (\Sentry\State\Scope $scope) use ($context) {
+            foreach ($context as $key => $value) {
+                $scope->setExtra("post.$key", $value);
+            }
+        });
+    }
 
     protected function getCategory()
     {
