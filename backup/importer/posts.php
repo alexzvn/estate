3b9da22164460c8e586a4edb93ab533a7ead181f
Alex <boytunghc@gmail.com>
2021-03-14 17:25:52 +0700

    [mysql-upgrade] fix price when import

--- backup/importer/posts.php
+++ backup/importer/posts.php
@@ -11,6 +11,20 @@ $type = function ($oid, $data) {
     return $mapped[$data['type']] ?? null;
 };
 
+$price = function ($oid, $data) {
+    $price = $data['price'];
+
+    if (! $price) {
+        return 0;
+    }
+
+    if ( !is_int($price) && $price !== null) {
+        return (int) $price->{'$numberLong'};
+    }
+
+    return $price;
+};
+
 return get('posts', new Mapper([
     'title' => 'string',
     'content' => 'string',
@@ -21,7 +35,7 @@ return get('posts', new Mapper([
     'approve_fee' => 'boolean',
     'commission' => 'string',
     'index_meta' => 'string',
-    'price' => 'int',
+    'price' => $price,
     'status' => 'int',
     'verifier_id' => 'id.users',
     'user_id' => 'id.users',
