be9fd90b07c95cef0212f9af385776c6db6e7bc1
Alex <alex.zdea@gmail.com>
2021-04-12 22:21:24 +0700

    add missing fields

--- backup/importer/blacklists.php
+++ backup/importer/blacklists.php
@@ -3,6 +3,9 @@
 return get('blacklists', new Mapper([
     'phone' => 'string',
     'user_id' => 'id.users',
+    'name' => 'string',
+    'url' => 'string',
+    'category' => 'string',
     'updated_at' => 'datetime',
     'created_at' => 'datetime',
 ]));
