dd681f6e80e8ae374fe32514f6bbdd6a7ec0dd3f
Alex <boytunghc@gmail.com>
2021-04-28 21:37:15 +0700

    add index prefix

--- app/Models/User.php
+++ app/Models/User.php
@@ -356,7 +356,7 @@ class User extends Authenticatable implements MustVerifyPhone, Auditable
     {
         $user = array_merge($this->toArray(), [
             'order.total' => $this->orders->sum('total'),
-            'subscriptions' => $this->subscriptions->compress(),
+            'subscriptions' => $this->subscriptions->compress('subscriptions.'),
             'post.seen' => $this->logs()->where('content', 'regexp', '^(Đã xem tin)')->exists(),
             'has_login' => $this->logs()->where('content', 'regexp', '^(Đã đăng nhập)')->exists()
         ]);
