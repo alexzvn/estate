d661c7135ffc2317bf1a12e7b83220875bffd605
Alex <boytunghc@gmail.com>
2021-05-13 12:39:14 +0700

    update title style

--- resources/views/customer/components/feature-popup.blade.php
+++ resources/views/customer/components/feature-popup.blade.php
@@ -14,11 +14,14 @@
     <div id="popup-info" class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog modal-md" role="document">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Thông báo</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                        </button>
+                <div class="modal-header tw-p-3">
+                    <p>&nbsp;</p>
+                    <div class="tw-text-center">
+                        <h5 class="modal-title tw-text-xl tw-text-red-500 tw-font-semibold tw-uppercase">Thông báo</h5>
+                    </div>
+                    <button class="tw-text-xl" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body ck-content">
                     {!! $setting->get($key) !!}
