19f657b016c3e019715f58a03051622b20f34a54
Alex <alex.zdea@gmail.com>
2021-05-17 21:56:45 +0700

    fix blade

--- resources/views/customer/me/history.blade.php
+++ resources/views/customer/me/history.blade.php
@@ -3,24 +3,26 @@
 @section('main-content')
 <div class="card">
     <div class="card-body">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Nội dung</th>
-                    <th>Lúc</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($logs as $log)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $log->content }}</td>
-                    <td>{{ $log->created_at->diffForHumans() }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nội dung</th>
+                        <th>Lúc</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($logs as $log)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $log->content }}</td>
+                        <td>{{ $log->created_at->diffForHumans() }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
