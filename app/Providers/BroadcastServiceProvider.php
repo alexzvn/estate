d278c3ca475416c7147f103f0877f00f3a19bb2f
Alex <boytunghc@gmail.com>
2021-06-13 23:32:21 +0700

    allowed serverless auth

--- app/Providers/BroadcastServiceProvider.php
+++ app/Providers/BroadcastServiceProvider.php
@@ -14,7 +14,7 @@ class BroadcastServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Broadcast::routes();
+        Broadcast::routes(['middleware' => 'auth.basic.once']);
 
         require base_path('routes/channels.php');
     }
