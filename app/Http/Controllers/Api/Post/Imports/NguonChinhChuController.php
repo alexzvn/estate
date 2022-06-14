dfe3d307fb2ac7141d8524d553e3a59446e650d9
Alex <41188285+alexzvn@users.noreply.github.com>
2022-06-14 09:26:27 +0000

    update category

--- app/Http/Controllers/Api/Post/Imports/NguonChinhChuController.php
+++ app/Http/Controllers/Api/Post/Imports/NguonChinhChuController.php
@@ -9,32 +9,12 @@ use App\Http\Controllers\Api\Post\ImportController;
 
 class NguonChinhChuController extends ImportController
 {
-    protected $mapped = [
-        'bán nhà mặt phố' => 'Bán nhà mặt phố',
-        'bán nhà trong ngõ' => 'Bán nhà riêng, trong ngõ',
-        'bán căn hộ, chung cư' => 'Bán căn hộ, chung cư',
-        'bán biệt thự, liền kề' => 'Bán biệt thự, liền kề, phân lô',
-        'bán nhà tập thể' => 'Bán nhà tập thể',
-        'bán đất' => 'Bán đất ở, đất thổ cư',
-        'bán kho, xưởng' => 'Bán kho, xưởng',
-
-        'cho thuê nhà mặt phố' => 'Cho thuê nhà mặt phố',
-        'cho thuê nhà trong ngõ' => 'Cho thuê nhà riêng, trong ngõ',
-        'cho thuê căn hộ, chung cư' => 'Cho thuê căn hộ, chung cư',
-        'cho thuê biệt thự, liền kề' => 'Cho thuê biệt thự, liền kề, phân lô',
-        'cho thuê nhà tập thể' => 'Cho thuê nhà tập thể',
-        'cho thuê kho, xưởng' => 'Cho thuê kho, xưởng',
-        'cho thuê cửa hàng - kiot' => 'Cho thuê văn phòng, mặt bằng kinh doanh',
-        'cho thuê văn phòng, mbkd' => 'Cho thuê văn phòng, mặt bằng kinh doanh',
-    ];
-
     public function queue(Collection $posts)
     {
         $posts->each(function ($post) {
            $hash = sha1($post->url);
            $phone = $this->normalizePhone($post->phone);
 
-           $post->category = $this->mapped[Str::lower($post->category)] ?? $post->category;
            $post->hash = $hash;
            $post->phone = $phone;
            $post->price = $this->normalizePrice($post->price);
--- app/Jobs/Post/ImportNguonChinhChuComJob.php
+++ app/Jobs/Post/ImportNguonChinhChuComJob.php
@@ -2,17 +2,38 @@
 
 namespace App\Jobs\Post;
 
+use Carbon\Carbon;
+use App\Enums\PostType;
+use App\Repository\Post;
 use App\Enums\PostSource;
 use App\Enums\PostStatus;
-use App\Enums\PostType;
+use Illuminate\Support\Str;
+use App\Services\System\Post\Fee;
 use App\Repository\Location\District;
 use App\Repository\Location\Province;
-use App\Repository\Post;
-use App\Services\System\Post\Fee;
-use Carbon\Carbon;
 
 class ImportNguonChinhChuComJob extends ImportTccJob
 {
+    protected $mapped = [
+        'bán nhà mặt phố' => 'Bán nhà mặt phố',
+        'bán nhà trong ngõ' => 'Bán nhà nhà riêng, trong ngõ',
+        'bán căn hộ, chung cư' => 'Bán căn hộ, chung cư',
+        'bán biệt thự, liền kề' => 'Bán biệt thự, liền kề, phân lô',
+        'bán nhà tập thể' => 'Bán nhà tập thể',
+        'bán đất' => 'Bán đất ở, đất thổ cư',
+        'bán kho, xưởng' => 'Bán kho, xưởng',
+
+        'cho thuê nhà mặt phố' => 'Cho thuê nhà mặt phố',
+        'cho thuê nhà trong ngõ' => 'Cho thuê nhà riêng, trong ngõ',
+        'cho thuê căn hộ, chung cư' => 'Cho thuê căn hộ, chung cư',
+        'cho thuê biệt thự, liền kề' => 'Cho thuê biệt thự, liền kề, phân lô',
+        'cho thuê nhà tập thể' => 'Cho thuê nhà tập thể',
+        'cho thuê kho, xưởng' => 'Cho thuê kho, xưởng',
+        'cho thuê cửa hàng - kiot' => 'Cho thuê văn phòng, mặt bằng kinh doanh',
+        'cho thuê văn phòng, mbkd' => 'Cho thuê văn phòng, mặt bằng kinh doanh',
+    ];
+
+
     /**
      * Execute the job.
      *
@@ -21,6 +42,7 @@ class ImportNguonChinhChuComJob extends ImportTccJob
     public function handle(Post $post)
     {
         $this->setReport((array) $this->post);
+        $this->post->category = $this->mapped[Str::lower($this->post->category)] ?? $this->post->category;
 
         if ($post->where('hash', $this->post->hash)->exists()) {
             return;
